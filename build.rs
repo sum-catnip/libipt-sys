8eef953c5c81e52550ca4c3bf4704bf3713d836f
catnip <catnip@catnip.fyi>
2020-01-09 10:42:02 +0100

    dynamic debug to static release build

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
-use cmake;
+use cmake::Config;
 
 fn main() {
-    let dst = cmake::build("libipt");
-    println!("cargo:rustc-link-lib=libipt");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("lib").to_str().unwrap()
-    );
+    let dst = Config::new("libipt")
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .build();
+
+    println!("cargo:rustc-link-lib=static=libipt");
+    println!("cargo:rustc-link-search=native={}",
+             dst.join("lib").to_str().unwrap());
 
     std::fs::copy(dst.join("include/intel-pt.h"), "intel-pt.h")
         .expect("intel-pt.h not found. build probably failed");
