a42aa5e8b5fb04bd1ca4ee5f78ad80fcf7b234b3
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2024-11-19 11:03:57 +0100

    Generate bindings at compile time

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 use cmake::Config;
+use std::env;
+use std::path::Path;
+
+const LIBIPT_SOURCE: &str = "libipt";
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    check_submodule(LIBIPT_SOURCE);
+
     let dst = Config::new("libipt")
         .define("BUILD_SHARED_LIBS", "OFF")
         .build();
@@ -10,9 +18,33 @@ fn main() {
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=static=ipt");
 
-    println!("cargo:rustc-link-search=native={}",
-             dst.join("lib").to_str().unwrap());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").to_string_lossy()
+    );
 
-    std::fs::copy(dst.join("include/intel-pt.h"), "intel-pt.h")
-        .expect("intel-pt.h not found. build probably failed");
+    let bindings = bindgen::Builder::default()
+        .header(dst.join("include").join("intel-pt.h").to_string_lossy())
+        .allowlist_function("pt_.*")
+        .allowlist_type("pt_.*")
+        .allowlist_var("pt_.*")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate libipt bindings");
+
+    bindings
+        .write_to_file(Path::new(&out_dir).join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+fn check_submodule(dir: &str) {
+    let path = Path::new(dir);
+    if !path.exists() || path.iter().next().is_none() {
+        let error = format!("{dir} directory not found or empty");
+        println!("cargo:warning={error}");
+        println!(
+            "cargo:warning=Hint: Please get the submodules with `git submodule update --init --recursive`"
+        );
+        panic!("{error}");
+    }
 }
