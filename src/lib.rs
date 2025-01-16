bed5ea9188501c754b3d9132aca778096b70ddfb
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2025-01-16 10:22:09 +0100

    silence Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::useless_transmute)]
+#![allow(clippy::too_many_arguments)]
+#![allow(clippy::ptr_offset_with_cast)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
